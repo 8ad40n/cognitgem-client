31fb6d8261dbf0c885f77713017c4066aafd10b9
8ad40n <joy184110@gmail.com>
2024-09-21 01:46:29 +0600

    fixed set and accordion

--- src/app/generate/page.jsx
+++ src/app/generate/page.jsx
@@ -86,9 +86,9 @@ export default function ChatPage() {
   return (
     <div className="mb-0">
       <div className="container mx-auto px-4 md:px-8">
-        <ScrollArea className="w-full h-[82vh] md:h-[72vh] rounded-md text-xs md:text-base">
+        <ScrollArea className="w-full h-[calc(100vh-150px)] md:h-[calc(100vh-170px)] rounded-md text-xs md:text-base">
           {session?.data?.user && (
-            <div className="text-center mb-4 mt-14 md:mt-20">
+            <div className="text-center mb-8 mt-14 md:mt-20">
               <TypingAnimation
                 className="text-xl md:text-2xl lg:text-3xl font-medium mb-3 text-black dark:text-white"
                 text={`Hello, ${session.data.user.name}`}
@@ -100,12 +100,9 @@ export default function ChatPage() {
           )}
 
           {conversation.map((chat, index) => (
-            <div
-              key={index}
-              className={`mb-4 flex items-start gap-4`} 
-            >
+            <div key={index} className={`mb-4 flex items-start gap-4`}>
               {chat.sender === "user" && (
-                <div className="flex items-start gap-2"> 
+                <div className="flex items-start gap-2">
                   <Image
                     src={session.data.user.image}
                     alt={session.data.user.name}
@@ -120,7 +117,7 @@ export default function ChatPage() {
               )}
 
               {chat.sender === "ai" && (
-                <div className="flex items-start gap-2"> 
+                <div className="flex items-start gap-2">
                   <Image
                     src="/assets/images/icon.png"
                     alt="AI"
@@ -128,13 +125,14 @@ export default function ChatPage() {
                     height={30}
                     className="rounded-full mb-2"
                   />
-                  <div className="inline-block text-white pl-2 pr-4 py-2 rounded-xl text-left">
+                  <div className="inline-block text-white pl-2 pr-6 py-2 rounded-xl text-left">
                     {renderMessage(chat.message)}
                   </div>
                 </div>
               )}
             </div>
           ))}
+
           {loading && (
             <div className="text-center text-gray-500 flex items-end justify-center">
               <p>Generating response...</p>
@@ -142,25 +140,24 @@ export default function ChatPage() {
           )}
         </ScrollArea>
 
-        <div className="flex justify-center items-center">
-          <form
-            onSubmit={handleForm}
-            className="flex items-center mt-4 w-full md:w-[90vw] lg:w-[70vw] xl:w-[60vw] mx-auto"
-          >
-            <input
-              type="text"
-              placeholder="Write your prompt..."
-              name="prompt"
-              className="flex-1 mr-2 h-10 md:h-12 lg:h-14 rounded-xl pl-4 bg-[#1e1f20] text-sm md:text-base"
-            />
-            <Button
-              type="submit"
-              className="h-10 md:h-12 lg:h-14 bg-[#1e1f20] border text-white rounded-xl"
-              disabled={loading}
-            >
-              <IoSend />
-            </Button>
-          </form>
+        <div className="fixed bottom-0 left-0 right-0 flex justify-center items-center w-full px-4 py-4 mb-2">
+          <div className="w-full md:w-[90vw] lg:w-[70vw] xl:w-[60vw]">
+            <form onSubmit={handleForm} className="flex items-center w-full">
+              <input
+                type="text"
+                placeholder="Write your prompt..."
+                name="prompt"
+                className="flex-1 mr-2 h-10 md:h-12 lg:h-14 rounded-xl pl-4 bg-[#1e1f20] text-sm md:text-base"
+              />
+              <Button
+                type="submit"
+                className="h-10 md:h-12 lg:h-14 bg-[#2f2f2f] border text-white rounded-xl"
+                disabled={loading}
+              >
+                <IoSend />
+              </Button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
